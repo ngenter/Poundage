b422ee3220dff6b6eb3fd6a2f565b6025512961f
Nathaniel Genter <njg401@yahoo.com>
2017-06-20 21:20:10 -0500

    1.0 Baby

--- src/app/hxw.component.ts
+++ src/app/hxw.component.ts
@@ -12,8 +12,10 @@ export class HxwComponent {
     questionFootage: number;
     questionPoundage: number;
     answer: number = null;
-
+    rollIs: string = null;
+    
     showAnswer: boolean = false;
+    showHelp: boolean = false;
     
     calculate(){
         if (this.questionFootage == null)
@@ -21,12 +23,14 @@ export class HxwComponent {
             this.answer = (this.knownFootage / this.knownPoundage * this.questionPoundage)
             console.log(this.answer)
             this.showAnswer = !this.showAnswer
+            this.rollIs = "feet"
         }
         
         else {
             this.answer = (this.knownPoundage / this.knownFootage * this.questionFootage)
             console.log(this.answer)
             this.showAnswer = !this.showAnswer
+            this.rollIs = "pounds"
         }
 
     }
@@ -38,6 +42,20 @@ export class HxwComponent {
 
     clearPoundage(){
         this.questionPoundage = null;
+    }
+
+    clearAll(){
+        this.knownFootage = null;
+        this.knownPoundage = null;
+        this.questionFootage = null;
+        this.questionPoundage = null;
+        this.answer = null;
+        this.showAnswer = false;
+        this.showHelp = false;
+    }
+
+    help(){
+        this.showHelp = true;
     }
 
     
